99b11db50f986a890e0765ed025755d40a4b1fd2
jbensal <jay.bensal@gmail.com>
2026-07-16 19:13:45 -0400

    Add hash router for temp fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { FluentProvider } from "@fluentui/react-components";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { maqLightTheme } from "./theme";
 import { App } from "./App";
 import "./styles.css";
@@ -9,9 +9,9 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <FluentProvider theme={maqLightTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </FluentProvider>
   </React.StrictMode>
 );
